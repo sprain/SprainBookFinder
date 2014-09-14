488374e6f6b2009825bf0badc5b3d6e4afcaf3cb
Manuel Reinhard <manu@sprain.ch>
2014-09-14 15:50:19 +0200

    Updated example

--- example.php
+++ example.php
@@ -2,15 +2,33 @@
 
 include 'vendor/autoload.php';
 
+use Symfony\Component\Intl\Intl;
+
 $providers = array(
     array(
-        'object' => new \Sprain\BookFinder\Providers\GoogleProvider('YOUR_GOOGLE_API_KEY'),
+        'provider' => new \Sprain\BookFinder\Providers\GoogleProvider('YOUR_GOOGLE_API_KEY'),
+        'name' => 'Google', // optional
+        'order' => 2 // optional, but recommended
     ),
     array(
-        'object' => new \Sprain\BookFinder\Providers\AmazonProvider('YOUR_AMAZON_ACCESS_KEY', 'YOUR_AMAZON_SECRET', 'YOUR_AMAZON_ASSOCIATE_TAG'),
-    )
+        'provider' => new \Sprain\BookFinder\Providers\AmazonProvider('YOUR_AMAZON_ACCESS_KEY', 'YOUR_AMAZON_SECRET', 'YOUR_AMAZON_ASSOCIATE_TAG'),
+        'name' => 'Amazon', // optional
+        'order' => 1 // optional, but recommended
+    ),
 );
 
 $bookFinder = new \Sprain\BookFinder\BookFinder($providers);
 
-var_dump($bookFinder->searchByIsbn('9781591840565')->getResults());
+// Look for "The Art of the Start"
+$successfulProvider = $bookFinder->searchByIsbn('9781591840565');
+var_dump(array(
+    $successfulProvider->getResults(),
+    $successfulProvider->getName()
+));
+
+// Look for "Darm mit Charme"
+$successfulProvider = $bookFinder->searchByIsbn('3843707111');
+var_dump(array(
+    $successfulProvider->getResults(),
+    $successfulProvider->getName()
+));
